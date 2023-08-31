1fc81bc3d41dfb84e10d20a2668bcf44b72dec5a
rafapereirabr <rafa.pereira.br@gmail.com>
2023-08-31 10:56:38 -0300

    new censobr_cache.R fnew documentation of cache parameter

--- man/roxygen/templates/cache.R
+++ man/roxygen/templates/cache.R
@@ -1,7 +1,7 @@
 #' @param cache Logical. The first time the user runs the function, `censobr`
-#'        will download the file from the internet and store it locally at
-#'        `tools::R_user_dir('censobr', 'cache')`, so that the file only needs
-#'        to be download once. When the `cache` parameter is set to `TRUE`
-#'        (Default), the function will read the data cached locally, which is
-#'        much faster. If `FALSE`, the function will download the file from the
-#'        internet again.
+#'        will download the file from the internet and store it locally so that
+#'        the file only needs to be download once. When the `cache` parameter is
+#'        set to `TRUE` (Default), the function will read the cached data, which
+#'        is much faster. If `FALSE`, the function will download the file from
+#'        the internet again and overwrite an eventual local file. The data is
+#'        stored locally at `tools::R_user_dir('censobr_v0.1', 'cache')`.
