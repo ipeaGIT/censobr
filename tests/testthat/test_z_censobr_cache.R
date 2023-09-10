760758ae19ef2646d00f18c8e833f28cc3c29da6
rafapereirabr <rafa.pereira.br@gmail.com>
2023-09-10 17:17:48 -0300

    fix test

--- tests/testthat/test_z_censobr_cache.R
+++ tests/testthat/test_z_censobr_cache.R
@@ -18,7 +18,7 @@ test_that("censobr_cache", {
   censobr::read_emigration(year = 2010, showProgress = FALSE, cache = TRUE)
 
   # cache dir
-  pkgv <- paste0('censobr_', data_release)
+  pkgv <- paste0('censobr/data_release_', data_release)
   cache_dir <- tools::R_user_dir(pkgv, which = 'cache')
 
   # list cached files
