09c49e8cb3963698439cd5635b72824738a90fe6
rafapereirabr <rafa.pereira.br@gmail.com>
2023-09-10 14:11:55 -0300

    cache inside censobr / version/

--- R/onLoad.R
+++ R/onLoad.R
@@ -7,8 +7,10 @@ censobr_env <- new.env(parent = emptyenv()) # nocov start
   censobr_env$data_release <- 'v0.1.0'
 
   # local cache dir
-  pkgv <- paste0('censobr_', censobr_env$data_release)
-  censobr_env$pkgv <- pkgv
-  censobr_env$cache_dir <- tools::R_user_dir(pkgv, which = 'cache')
+  censobr_env$cache_dir <- tools::R_user_dir('censobr', which = 'cache')
+  censobr_env$cache_dir <- paste0(censobr_env$cache_dir,'/',censobr_env$data_release)
   # gsub("\\\\", "/", censobr_env$cache_dir)
+
+
+
 } # nocov end
