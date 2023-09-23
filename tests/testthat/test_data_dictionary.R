9a9d0797e6367a462acb7ff5a80116d468559753
rafapereirabr <rafa.pereira.br@gmail.com>
2023-09-23 15:43:31 -0300

    tests for data dictionary

---
+++ tests/testthat/test_data_dictionary.R
@@ -0,0 +1,58 @@
+context("data_dictionary")
+
+# skip tests because they take too much time
+skip_if(Sys.getenv("TEST_ONE") != "")
+testthat::skip_on_cran()
+
+
+# Reading the data -----------------------
+
+test_that("data_dictionary", {
+
+  # tracts
+  testthat::expect_message( data_dictionary(year = 2010, dataset = 'tracts') )
+  testthat::expect_message( data_dictionary(year = 2000, dataset = 'tracts') )
+  testthat::expect_message( data_dictionary(year = 1991, dataset = 'tracts') )
+  testthat::expect_message( data_dictionary(year = 1980, dataset = 'tracts') )
+  testthat::expect_message( data_dictionary(year = 1970, dataset = 'tracts') )
+
+  # population
+  testthat::expect_message( data_dictionary(year = 2010, dataset = 'population') )
+  testthat::expect_message( data_dictionary(year = 2000, dataset = 'population') )
+  testthat::expect_error( data_dictionary(year = 1991, dataset = 'population') )
+  testthat::expect_message( data_dictionary(year = 1980, dataset = 'population') )
+  testthat::expect_message( data_dictionary(year = 1970, dataset = 'population') )
+
+  # households
+  testthat::expect_message( data_dictionary(year = 2010, dataset = 'households') )
+  testthat::expect_message( data_dictionary(year = 2000, dataset = 'households') )
+  testthat::expect_error( data_dictionary(year = 1991, dataset = 'population') )
+  testthat::expect_message( data_dictionary(year = 1980, dataset = 'households') )
+  testthat::expect_error( data_dictionary(year = 1970, dataset = 'households') )
+
+  # families
+  testthat::expect_message( data_dictionary(year = 2000, dataset = 'families') )
+  testthat::expect_error( data_dictionary(year = 1991, dataset = 'families') )
+
+  # mortality
+  testthat::expect_message( data_dictionary(year = 2010, dataset = 'mortality') )
+  testthat::expect_error( data_dictionary(year = 1991, dataset = 'mortality') )
+
+  # emigration
+  testthat::expect_message( data_dictionary(year = 2010, dataset = 'emigration') )
+  testthat::expect_error( data_dictionary(year = 1991, dataset = 'emigration') )
+
+
+
+ })
+
+
+# ERRORS and messages  -----------------------
+test_that("data_dictionary", {
+
+  # Wrong date 4 digits
+  testthat::expect_error( data_dictionary(year = 1991, dataset = 'banana') )
+  testthat::expect_error( data_dictionary(year = banana, dataset = 'population') )
+})
+
+
