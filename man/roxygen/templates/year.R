6dabd116cee8c19b4bba6501857a796239954860
rafapereirabr <rafa.pereira.br@gmail.com>
2024-02-05 20:11:06 -0300

    update year template

--- man/roxygen/templates/year.R
+++ man/roxygen/templates/year.R
@@ -1,2 +1,1 @@
-#' @param year Numeric. Year of reference in the format `yyyy`. Defaults to
-#'             `2010`.
+#' @param year Numeric. Year of reference in the format `yyyy`. Defaults to `2010`.
