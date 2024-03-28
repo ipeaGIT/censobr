12824152da24b323d2147f21d985e6256a7109ba
rafapereirabr <rafa.pereira.br@gmail.com>
2024-03-27 22:33:45 -0300

    prep census_tracts_aggreg_2022_prelim, col classes

--- data_prep/R/census_tracts_aggreg_2022_prelim.R
+++ data_prep/R/census_tracts_aggreg_2022_prelim.R
@@ -11,12 +11,10 @@ df[, CD_SETOR := gsub("P","", CD_SETOR)]
 head(df)
 
 
-# make all columns as character
-df <- mutate(df, across(everything(), as.character))
-
+# rename columns
 names(df) <- toupper(names(df))
 
-df2 <- dplyr::rename(df,
+df2 <- dplyr::select(df,
               code_tract = CD_SETOR,
               code_muni = CD_MUN,
               name_muni = NM_MUN,
@@ -38,9 +36,22 @@ df2 <- dplyr::rename(df,
               name_intermediate = NM_RGINT,
               code_region = CD_REGIAO,
               name_region = NM_REGIAO,
+              V0001 = V0001,
+              V0002 = V0002,
+              V0003 = V0003,
+              V0004 = V0004,
+              V0005 = V0005,
+              V0006 = V0006,
+              V0007 = V0007,
               area_km2 = AREA_KM2
               )
 head(df2)
+
+# make all columns as character
+character_cols <- names(df2)[names(df2) %like% 'code_|name_']
+df2 <- mutate(df2, across(everything(character_cols), as.character))
+
+sapply(df2, class)
 
 
 # save
@@ -49,6 +60,6 @@ dir.create('./data/tracts/2022/', recursive = T)
 dest_file <- paste0('2022_tracts_Preliminares.parquet')
 arrow::write_parquet(df2, paste0('./data/tracts/2022/', dest_file))
 
-arrow::write_parquet(df2, 'd2.parquet')
+
 
 
