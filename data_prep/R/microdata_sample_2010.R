16449864e0435ee6f91b6db4b386001b4daeec19
rafapereirabr <rafa.pereira.br@gmail.com>
2024-07-16 20:58:25 -0300

    testing schema for 2010 col classes

--- data_prep/R/microdata_sample_2010.R
+++ data_prep/R/microdata_sample_2010.R
@@ -21,6 +21,7 @@ source('./R/add_geography_cols.R')
   dic <- fread('./read_guides/readguide_2010_households.csv')
   head(dic)
 
+
   # list all files
   txt_files <- list.files('R:/Dropbox/bases_de_dados/censo_demografico/censo_2010/data/dados_txt2010/Microdados',
                           pattern = 'Domicilios_[[:digit:]]',
@@ -36,23 +37,52 @@ source('./R/add_geography_cols.R')
                             recursive = TRUE,
                             full.names = TRUE)
 
+  # building schema: unite columns
+  dic[, col_class := fcase(decimal_places>0, 'numeric(),',
+                           between(length, 1, 2) & is.na(decimal_places), 'uint8(),',
+                           between(length, 3, 4) & is.na(decimal_places), 'uint8(),',
+                           between(length, 5, 9) & is.na(decimal_places), 'uint32(),',
+                           length > 9 & is.na(decimal_places), 'uint64(),',
+                           default= 'string(),')]
+
+  schema_tbl <- dic[,. (var_name, col_class)] |> unique()
+  u <- tidyr::unite(schema_tbl, col = 'col_class', sep = ' = ')
+
+  # build text expression
+  schema_string <- paste0('arrow::schema(code_muni = int32(), code_state = int8(),abbrev_state = string(),name_state = string(),code_region = int8(),name_region = string(),code_weighting = string(),',
+                          paste0(unlist(u$col_class), collapse = " "),
+                          ')')
+
+  # remove last comma
+  schema_string <- gsub(",)$", ")", schema_string)
+
   # Define the dataset
-  DS <- arrow::open_dataset(sources = parqt_files)
-  # Create a scanner
-  SO <- Scanner$create(DS)
-  # Load it as n Arrow Table in memory
-  AT <- SO$ToTable()
-  rm(DS, SO); gc(T)
+  ds <- arrow::open_dataset(sources = parqt_files)
 
   ## 3.3) add geography variables ----------------------------------------------
-  AT <- add_geography_cols(arrw = AT, year = 2010)
+  AT <- add_geography_cols(arrw = ds, year = 2010)
 
-  head(AT) |> collect()
+  # apply schema
+  AT <- arrow::as_arrow_table(AT,
+                              schema = eval(str2lang(schema_string))
+                              )
+
+
+  schema(AT)
 
   ## 3.4) save single parquet tile ----------------------------------------------
-  arrow::write_parquet(AT, './data/microdata_sample/2010/2010_households.parquet')
+  # AT |> collect() |>
+  arrow::write_parquet(AT, './data/microdata_sample/2010/2010_households_uINT3.parquet')
 
 
+
+
+# a <- arrow::open_dataset('./data/microdata_sample/2010/2010_households_INT.parquet')
+# schema(a)
+#
+# b <- arrow::open_dataset('./data/microdata_sample/2010/2010_households_v0.3.0.parquet')
+# schema(b)
+#
 
 
 
@@ -80,28 +110,55 @@ source('./R/add_geography_cols.R')
                             recursive = TRUE,
                             full.names = TRUE)
 
+  # building schema: unite columns
+  dic[, col_class := fcase(decimal_places>0, 'numeric(),',
+                           between(length, 1, 2) & is.na(decimal_places), 'int8(),',
+                           between(length, 3, 4) & is.na(decimal_places), 'int16(),',
+                           between(length, 5, 9) & is.na(decimal_places), 'int32(),',
+                           length > 9 & is.na(decimal_places), 'int64(),',
+                           default= 'string(),')]
+
+  schema_tbl <- dic[,. (var_name, col_class)] |> unique()
+  u <- tidyr::unite(schema_tbl, col = 'col_class', sep = ' = ')
+
+  # build text expression
+  schema_string <- paste0('arrow::schema(code_muni = int32(), code_state = int8(),abbrev_state = string(),name_state = string(),code_region = int8(),name_region = string(),code_weighting = string(),',
+                          paste0(unlist(u$col_class), collapse = " "),
+                          ')')
+
+  # remove last comma
+  schema_string <- gsub(",)$", ")", schema_string)
+
+
   # Define the dataset
-  DS <- arrow::open_dataset(sources = parqt_files)
-  # Create a scanner
-  SO <- Scanner$create(DS)
-  # Load it as n Arrow Table in memory
-  AT <- SO$ToTable()
-  rm(DS, SO); gc(T)
+  ds <- arrow::open_dataset(sources = parqt_files)
 
   ## 4.3) add geography variables ----------------------------------------------
-  AT <- add_geography_cols(arrw = AT, year = 2010)
+  AT <- add_geography_cols(arrw = ds, year = 2010)
 
-  head(AT) |> collect()
+  # apply schema
+  AT <- arrow::as_arrow_table(AT,
+                              schema = eval(str2lang(schema_string))
+  )
+
+
+  schema(AT)
 
   ## 4.4) save single parquet tile ----------------------------------------------
-  arrow::write_parquet(AT, './data/microdata_sample/2010/2010_population.parquet')
+  arrow::write_parquet(AT, './data/microdata_sample/2010/2010_population_INT.parquet')
+
+
+a <-   arrow::open_dataset('./data/microdata_sample/2010/2010_population_v0.3.0.parquet'
+                          # , schema = s
+                           )
+
+schema(a)
 
 
 
+ b<-  arrow::open_dataset('./data/microdata_sample/2010/2010_population_INT.parquet')
 
-
-
-
+schema(b)
 
 
 # 5) Mortality data ----------------------------------------------
