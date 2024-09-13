a7f1c5bde20eee1103cece389f2f8638cb2a6e70
rafapereirabr <rafa.pereira.br@gmail.com>
2024-09-13 10:41:40 -0300

    tests merge household

--- R/merge_household.R
+++ R/merge_household.R
@@ -74,20 +74,16 @@ merge_household_var <- function(df,
   df_household <- dplyr::filter(df_household, get(key_key) %in% key_values) |>
                   dplyr::compute()
 
-#  nrow(df_household)
-#  [1] 6192332
-
   # convert to duckdb
   # df <- arrow::to_duckdb(df)
   # df_household <- arrow::to_duckdb(df_household)
 
-  # con <- DBI::dbConnect(duckdb::duckdb(), read_only = FALSE)
   con <- duckdb::dbConnect(duckdb::duckdb(), read_only = FALSE)
   duckdb::duckdb_register_arrow(con, 'df', df)
   duckdb::duckdb_register_arrow(con, 'df_household', df_household)
 
   # limit RAM and threads of duckdb ???
-  # dbExecute(con, "PRAGMA threads=1; PRAGMA memory_limit='1GB';")
+  # DBI::dbExecute(con, "PRAGMA threads=1; PRAGMA memory_limit='1GB';")
   # dbExecute(conn = conn, paste0("PRAGMA memory_limit='12GB'"))
   # appears to work.
   # https://github.com/duckdb/duckdb-r/issues/83
@@ -95,13 +91,13 @@ merge_household_var <- function(df,
 
   # merge
   df_geo <- duckplyr::left_join(dplyr::tbl(con, "df"),
-                                dplyr::tbl(con, "df_household"))
+                                dplyr::tbl(con, "df_household"),
+                                by = key_vars)
 
   df_geo <- dplyr::compute(df_geo)
 
   # back to arrow
   df_geo <- arrow::to_arrow(df_geo)
-  df_geo <- dplyr::compute(df_geo)
 
   # remove duckdb instance
   duckdb::duckdb_unregister_arrow(con, 'df')
@@ -112,3 +108,106 @@ merge_household_var <- function(df,
 
   return(df_geo)
 }
+# merge_household_var <- function(df,
+#                                 year = parent.frame()$year,
+#                                 add_labels = parent.frame()$add_labels,
+#                                 showProgress = parent.frame()$showProgress){
+#
+#   # download household data
+#   df_household <- censobr::read_households(year = year,
+#                                            add_labels = add_labels,
+#                                            as_data_frame = FALSE,
+#                                            showProgress = showProgress)
+#
+#   # set vars to merge
+#   if (year == 1970) {
+#     key_vars <- c('code_muni', 'code_state', 'abbrev_state','name_state',
+#                   'code_region', 'name_region', 'id_household')
+#     key_key <- 'id_household'
+#   }
+#
+#   if (year == 1980) {
+#     key_vars <- c('code_muni', 'code_state', 'abbrev_state','name_state',
+#                   'code_region', 'name_region', 'V6', 'V601')
+#     key_key <- 'V601'
+#
+#     # rename weight var
+#     df_household <- dplyr::rename(df_household, 'V603_household' = 'V603')
+#   }
+#
+#   if (year == 1991) {
+#     key_vars <- c('code_muni', 'code_state', 'abbrev_state','name_state',
+#                   'code_region', 'name_region', 'V0109')
+#
+#     key_key <- 'V0109'
+#     # rename weight var
+#     df_household <- dplyr::rename(df_household, 'V7300_household' = 'V7300')
+#   }
+#
+#   if (year == 2000) {
+#     key_vars <- c('code_muni', 'code_state', 'abbrev_state','name_state',
+#                   'code_region', 'name_region', 'code_weighting', 'V0300')
+#     key_key <- 'V0300'
+#   }
+#
+#   if (year == 2010) {
+#     key_vars <- c('code_muni', 'code_state', 'abbrev_state','name_state',
+#                   'code_region', 'name_region', 'code_weighting', 'V0300')
+#
+#     key_key <- 'V0300'
+#     # rename weight var
+#     df_household <- dplyr::rename(df_household, 'V0010_household' = 'V0010') |>
+#       dplyr::compute()
+#   }
+#
+#
+#   # drop repeated vars
+#   all_common_vars <- names(df)[names(df) %in% names(df_household)]
+#   vars_to_drop <- setdiff(all_common_vars, key_vars)
+#   df_household <- dplyr::select(df_household, -all_of(vars_to_drop)) |>
+#     dplyr::compute()
+#
+#   # # pre-filter right-hand table that matches key in left-hand table
+#   # this improves performance a bit
+#   df <- dplyr::compute(df)
+#   key_values <- as.vector(unique(df$GetColumnByName(key_key)))
+#   df_household <- dplyr::filter(df_household, get(key_key) %in% key_values) |>
+#     dplyr::compute()
+#
+#
+#   # con <- DBI::dbConnect(duckdb::duckdb(), read_only = FALSE)
+#   con <- duckdb::dbConnect(duckdb::duckdb(), read_only = FALSE, dbdir = "duckdb")
+#
+#   duckdb::duckdb_register_arrow(con, 'df', df)
+#   duckdb::duckdb_register_arrow(con, 'df_household', df_household)
+#
+#   # limit RAM and threads of duckdb ???
+#   # dbExecute(con, "PRAGMA threads=1; PRAGMA memory_limit='1GB';")
+#   # dbExecute(conn = conn, paste0("PRAGMA memory_limit='12GB'"))
+#   # appears to work.
+#   # https://github.com/duckdb/duckdb-r/issues/83
+#   # https://github.com/duckdb/duckdb-r/issues/72
+#
+#   df_db <- dplyr::tbl(con, "df")
+#   df_household_db <- dplyr::tbl(con, "df_household")
+#
+#   # merge
+#   df_geo <- dplyr::left_join(
+#     x = df_db,
+#     y = df_household_db,
+#     by = key_vars
+#   )
+#   df_geo <- dplyr::compute(df_geo)
+#
+#   # back to arrow
+#   df_geo <- arrow::to_arrow(df_geo)
+#
+#   # remove duckdb instance
+#   duckdb::duckdb_unregister_arrow(con, 'df')
+#   duckdb::duckdb_unregister_arrow(con, 'df_household')
+#   duckdb::dbDisconnect(con, shutdown = TRUE)
+#
+#   on.exit(duckdb::dbDisconnect(con, shutdown = TRUE))
+#
+#   return(df_geo)
+# }
--- tests/tests_rafa/test_duckdb_merge2.R
+++ tests/tests_rafa/test_duckdb_merge2.R
@@ -1,6 +1,8 @@
 library(arrow)
 library(duckdb)
 library(duckplyr)
+library(dplyr)
+library(dbplyr)
 library(DBI)
 
 # download parquet files
@@ -38,7 +40,7 @@ merge_fun <- function(arrow1, arrow2){
 
   # merge
   df_merge <- duckplyr::left_join(dplyr::tbl(con, "arrow1"),
-                                  dplyr::tbl(con, "arrow2"))
+                                  dplyr::tbl(con, "arrow2"),)
 
   df_merge <- dplyr::compute(df_merge)
 
