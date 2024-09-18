6b49757e4c5594fb40c1fe44e778824f8c33c407
rafapereirabr <rafa.pereira.br@gmail.com>
2024-09-17 23:44:05 -0300

    closes #32

--- data_prep/R/microdata_sample_1960.R
+++ data_prep/R/microdata_sample_1960.R
@@ -1,26 +1,27 @@
 library(arrow)
 library(dplyr)
+library(fst)
 
 source('./R/add_geography_cols.R')
 
+hh <- fst::read_fst('R:/Dropbox/censoBR/1960/microdados/Censo.1960.brasil.domicilios.amostraCompilada.censobr.fst')
+pop <- arrow::open_dataset('R:/Dropbox/censoBR/1960/microdados/Censo.1960.brasil.pessoas.amostraCompilada.censobr.parquet')
+pop <- collect(pop)
 
+gc(T)
 
-pop <- arrow::open_dataset('../../censobr_data_prep/data_raw/microdata/1960/1960_population.parquet')
-hh <- arrow::open_dataset('../../censobr_data_prep/data_raw/microdata/1960/1960_households.parquet')
 
 names(hh)
+names(pop)
 
 
-
+# rename columns
 pop <- pop |>
-  rename_with(~ ifelse(startsWith(., "v"), sub("^v", "V", .), .)) |>
-  rename(code_muni_1960 = code_muni)
-
+  rename_with(~ ifelse(startsWith(., "v"), sub("^v", "V", .), .))
 
 
 hh <- hh |>
-  rename_with(~ ifelse(startsWith(., "v"), sub("^v", "V", .), .)) |>
-  rename(code_muni_1960 = code_muni)
+  rename_with(~ ifelse(startsWith(., "v"), sub("^v", "V", .), .))
 
 
 names(pop)
