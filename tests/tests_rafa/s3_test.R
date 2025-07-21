4a1495cef75d583a19eb4ab6f7ce100ebbb141c0
rafapereirabr <rafa.pereira.br@gmail.com>
2025-07-21 17:53:21 -0300

    s3 test

---
+++ tests/tests_rafa/s3_test.R
@@ -0,0 +1,49 @@
+# https://github.com/cboettig/minioclient
+
+library(arrow)
+library(dplyr)
+
+minio <- arrow::S3FileSystem$create(
+  access_key = "CC10HvyOMmynA1bG12CD",
+  secret_key = "vKpyxfbKFDiR9aV23hHD832thlFDJXzYiobKwHdl",
+  scheme = "https",
+  endpoint_override = "minioapi-teste.ipea.gov.br:443",  # Or public DNS
+)
+
+
+
+
+Sys.setenv(
+  AWS_REGION = "us-east-1",
+  AWS_EC2_METADATA_DISABLED = "TRUE"
+)
+
+df <- arrow::open_dataset(
+  'censobr/2010_tracts_Pessoa_v0.5.0.parquet',
+  filesystem=minio, format="parquet"
+)
+
+nrow(df)
+
+# carrega pra memoria a base completa (310,114 observacoes)
+# pico de 66% de rede
+df2 <- df|> collect()
+
+# carrega pra memoria soh estado do acre (2,346 observacoes)
+# pico de 34% de rede
+df3 <- df |>
+  filter(code_state=='11') |>
+  collect()
+
+
+
+# https://github.com/cboettig/minioclient
+library(minioclient)
+
+mc_alias_set("censobr", "minioapi-teste.ipea.gov.br:443",
+             access_key = "CC10HvyOMmynA1bG12CD",
+             secret_key = "vKpyxfbKFDiR9aV23hHD832thlFDJXzYiobKwHdl"
+             )
+
+
+
