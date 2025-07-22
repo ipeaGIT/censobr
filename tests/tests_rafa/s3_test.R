8365afaf4abb8455906b968bc8b8e5a72701c510
rafapereirabr <rafa.pereira.br@gmail.com>
2025-07-22 15:34:30 -0300

    s3 tests

--- tests/tests_rafa/s3_test.R
+++ tests/tests_rafa/s3_test.R
@@ -7,10 +7,9 @@ minio <- arrow::S3FileSystem$create(
   access_key = "CC10HvyOMmynA1bG12CD",
   secret_key = "vKpyxfbKFDiR9aV23hHD832thlFDJXzYiobKwHdl",
   scheme = "https",
-  endpoint_override = "minioapi-teste.ipea.gov.br:443",  # Or public DNS
+#  endpoint_override = "minioapi-teste.ipea.gov.br:443",  # Or public DNS
+ endpoint_override = "minio.ipea.gov.br:443",  # Or public DNS
 )
-
-
 
 
 Sys.setenv(
@@ -46,4 +45,28 @@ mc_alias_set("censobr", "minioapi-teste.ipea.gov.br:443",
              )
 
 
+
+# ---------------------------------------------------------------------------------
+
+a <- "https://github.com/ipeaGIT/censobr/releases/download/v0.5.0/1960_households_v0.5.0.parquet"
+
+a <- "http://minio.ipea.gov.br/censobr/2010_tracts_Pessoa_v0.5.0.parquet"
+
+library(duckdbfs)
+
+ds <- duckdbfs::open_dataset(a)
+nrow(ds)
+
+ds2 <- duckdbfs::open_dataset(a, mode = "TABLE")
+class(ds2)
+nrow(ds2)
+ds2
+
+url <- "https://github.com/ipeaGIT/geobr/releases/download/v1.7.0/amazonia_legal.gpkg"
+
+countries_meta <- duckdbfs::st_read_meta(url )
+countries_meta
+
+g <- duckdbfs::open_dataset(url,format = "sf")
+in_mem <- g |> to_sf(crs = countries_meta$wkt)
 
